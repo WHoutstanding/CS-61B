82a658932b28bc732cc0827009aeef9bba82eb45
Wanghao <wh15839417029@outlook.com>
2024-07-26 14:59:12 +0800

    第二次提交

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static double G = 6.67e-11;
+    private static double G = 6.67e-11;
 
     public Planet (double xP, double yP, double xV,
                     double yV, double m, String img) {
@@ -14,7 +14,7 @@ public class Planet {
         xxVel = xV;
         yyVel = yV;
         mass = m;
-        imgFileName = "./images/" + img;
+        imgFileName =  img;
     }
 
     public Planet(Planet p){
@@ -98,6 +98,7 @@ public class Planet {
     }
 
     public void draw() {
-        StdDraw.picture(xxPos, yyPos, imgFileName);
+        String path = "./images/" + imgFileName;
+        StdDraw.picture(xxPos, yyPos, path);
     }
 }
