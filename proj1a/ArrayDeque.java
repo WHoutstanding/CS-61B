1f7d9db695e8cf111afa6e5d27463d8c89edbe3d
Wanghao <wh15839417029@outlook.com>
2024-07-29 22:07:32 +0800

    第一次提交

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,137 @@
+public class ArrayDeque<T> {
+    T[] items;
+    int size;
+    int nextFirst;
+    int nextLast;
+
+    /** 初始化ArrayDeque */
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst= 0;
+        nextLast = 1;
+    }
+
+    /** 增大items的大小 */
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a,0, nextFirst);
+        System.arraycopy(items, nextFirst, a, a.length-items.length+nextFirst ,items.length-nextFirst);
+        nextFirst = a.length-items.length+nextFirst;
+        items = a;
+    }
+
+    /** 在items的前面添加item */
+    public void addFirst(T item) {
+        size += 1;
+        if (nextFirst == nextLast) {
+            resize(2*size);
+        }
+
+        items[nextFirst--] = item;
+        if(nextFirst == -1) {
+            nextFirst = items.length-1;
+        }
+    }
+
+    /** 在items的后面添加item */
+    public void addLast(T item) {
+        size += 1;
+        if (nextLast == nextFirst) {
+            resize(2*size);
+        }
+
+        items[nextLast++] = item;
+        if(nextLast == items.length) {
+            nextLast = 0;
+        }
+    }
+
+    /** 判断items是否为空 */
+    public boolean isEmpty() {
+        if (size == 0) return true;
+        return false;
+    }
+
+    /** 返回items的大小 */
+    public int size() {
+        return size;
+    }
+
+    /** 从前到后打印items的内容*/
+    public void printDeque() {
+        for (int i = nextFirst+1; i != nextLast; i++) {
+            if (i == items.length) {
+                i = 0;
+            }
+            System.out.print(items[i] + " ");
+        }
+    }
+
+    /** 打印items的所有内容
+    public void printAll() {
+        for(int i = 0; i < items.length; i++) {
+            System.out.print(items[i] + " ");
+        }
+    }
+    */
+
+    /** 打印nextFirst和nextLast
+    public void printindex() {
+        System.out.print(nextFirst + " ");
+        System.out.print(nextLast + "\n");
+    }
+     */
+
+    /** 删除items的第一个item */
+    public T removeFirst() {
+        if (size == 0) return null;
+        size --;
+        nextFirst ++;
+        if(nextFirst == items.length) nextFirst = 0;
+        return items[nextFirst];
+
+    }
+
+    /** 删除items的最后一个item */
+    public T removeLast() {
+        if (size == 0) return null;
+        size--;
+        nextLast --;
+        if (nextLast == -1) nextLast = items.length-1;
+
+        return items[nextLast];
+    }
+
+    /** 返回第index个item */
+    public T get(int index) {
+        if (size == 0) return null;
+        return items[(index+1+nextFirst)%items.length];
+    }
+
+    /** 测试
+    public static void main(String[] args) {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer>();
+        L.addFirst(0);
+        L.addLast(1);
+        L.addFirst(2);
+        L.addLast(3);
+        L.addFirst(4);
+        L.addLast(5);
+        L.addFirst(6);
+        L.addLast(7);
+        L.addFirst(8);
+        L.addLast(9);
+        L.removeFirst();
+        L.removeLast();
+        L.removeFirst();
+        L.removeLast();
+        L.removeFirst();
+        L.removeLast();
+        L.removeFirst();
+        L.removeLast();
+        L.printDeque();
+//        System.out.println(L.get(9));
+    }
+     */
+}
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,144 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+/** Deque */
+public class LinkedListDeque<T> {
+
+    /** Node */
+    private class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node(Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private Node sentFront;
+    private int size;
+    private Node sentBack;
+
+    /** Constructor */
+    public LinkedListDeque() {
+        sentBack = new Node(null, null, null);
+        size = 0;
+        sentFront = new Node(null, null, null);
+        sentFront.next = sentBack;
+        sentBack.prev = sentFront;
+    }
+
+    /** Add item to the front of the Deque. */
+    public void addFirst(T item) {
+        size += 1;
+        Node n = new Node(null, item, null);
+        n.prev = sentFront;
+        n.next = sentFront.next;
+        sentFront.next.prev = n;
+        sentFront.next = n;
+    }
+
+    /** Add item to the last of the Deque. */
+    public void addLast(T item) {
+        size += 1;
+        Node n = new Node(null, item, null);
+        n.next = sentBack;
+        n.prev = sentBack.prev;
+        sentBack.prev.next = n;
+        sentBack.prev = n;
+    }
+
+    /** Is or is not empty. */
+    public boolean isEmpty() {
+        if (size == 0) return true;
+        return false;
+    }
+
+    /** Returns the size of Deque. */
+    public int size() {
+        return size;
+    }
+
+    /** print all items of the Deque. */
+    public void printDeque() {
+        Node q = sentFront;
+        while (q.next.next != null) {
+            q = q.next;
+            System.out.print(q.item + " ");
+        }
+    }
+
+    /** Remove the first node of the Deque and Reutrn its item
+     * (if exists) else return null. */
+    public T removeFirst() {
+        if (isEmpty()) return null;
+
+        size -= 1;
+        T item = sentFront.next.item;
+        sentFront.next.next.prev = sentFront;
+        sentFront.next = sentFront.next.next;
+
+        return item;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) return null;
+
+        size -= 1;
+        T item = sentBack.prev.item;
+        sentBack.prev.prev.next = sentBack;
+        sentBack.prev = sentBack.prev.prev;
+
+        return item;
+    }
+
+    public T get(int idex) {
+        if (isEmpty()) return null;
+
+        if (idex < 0 || idex >= size)  return null;
+
+        Node q = sentFront;
+        int cnt = 0;
+        while (q.next.next != null) {
+            q = q.next;
+            if (cnt == idex) break;
+            cnt += 1;
+        }
+
+        return q.item;
+    }
+
+    private T getRecursive(int cnt, int idex, Node n) {
+        if(cnt == idex) return n.item;
+
+        return getRecursive(cnt + 1, idex, n.next);
+    }
+
+
+    public T getRecursive(int idex) {
+        if (isEmpty()) return null;
+
+        if (idex < 0 || idex >= size) return null;
+
+        return getRecursive(0, idex, sentFront.next);
+    }
+
+    /** 测试
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> q = new LinkedListDeque();
+        q.addFirst(1);
+        q.addFirst(2);
+        q.addFirst(3);
+        q.addFirst(4);
+        q.addLast(0);
+        q.removeFirst();
+        q.removeLast();
+//        System.out.println(q.getFirst());
+//        System.out.println(q.getLast());
+//        System.out.println(q.isEmpty());
+//        System.out.println(t);
+        q.printDeque();
+//        System.out.println(q.getRecursive(3));
+    }
+     */
+
 }
