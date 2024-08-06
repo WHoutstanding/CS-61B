e668a0a4a39b0ecd445084c51b125e410d797a92
Wanghao <wh15839417029@outlook.com>
2024-08-06 15:05:34 +0800

    第一次提交

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    /** Test the methods of class StudentArrayDequeLauncher. */
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> input1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> input2 = new ArrayDequeSolution<>();
+
+        int N = 16;
+        for (int i = 0; i < N; i += 1) {
+            input1.addLast(i);
+            input2.addLast(i);
+            if (i % 2 != 0) {
+                input1.removeFirst();
+                input2.removeFirst();
+            }
+            int actual = input1.get(0);
+            int expected = input2.get(0);
+            assertEquals("\naddLast(" + i + ")" + "\n"
+                    + "removeFirst(" + i + ")" + "\n"
+                    + "removeFirst()", expected, actual);
+        }
+    }
 }
